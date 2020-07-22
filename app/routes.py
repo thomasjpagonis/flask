39c8c0de2605747081c1b3df44719e8964f616ba
Thomas Pagonis <thomasjpagonis@gmail.com>
2020-07-21 23:56:42 -0500

    Added registering users

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app
-from app.forms import LoginForm
+from app import db
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user,login_required
 from app.models import User
+from werkzeug.urls import url_parse
 
 # Methods - tell what type of requests this page accepts (default get only)
 # GET - the server returns data to the client
@@ -70,4 +72,16 @@ def index():
     return render_template('index.html',title = 'Home Page',posts=posts)
 
 
-
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html',title='Rigister',form=form)
