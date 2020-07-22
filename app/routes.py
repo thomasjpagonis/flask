725a395a06aab39ec743f504c0960e1c991c3269
Thomas Pagonis <thomasjpagonis@gmail.com>
2020-07-21 20:05:31 -0500

    Updated

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+from app.models import User
 
 # Methods - tell what type of requests this page accepts (default get only)
 # GET - the server returns data to the client
@@ -8,19 +10,44 @@ from app.forms import LoginForm
 
 @app.route('/login', methods=['GET','POST'])
 def login():
+
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     form = LoginForm()
 
     # When the browser submits a POST request the validate_on_submit() will check the forms validators
     if form.validate_on_submit():
         
-        flash('Login reguested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        # Search the SQL data base for the user
+        user = User.query.filter_by(username = form.username.data).first()
+
+        # If the username is none or if the password hash did not match then leave them on the login
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid Username or Password')
+            return redirect(url_for('login'))
+
+        # If they match then log them in
+        login_user(user, remember=form.remember_me.data)
+        
+
+
+
         return redirect(url_for('index'))
 
     return render_template('login.html', title ='Sign In', form=form)
 
 
+@app.route('/logout')
+def logout()
+    logout_user()
+    return redirect(url_for('index')
+
+
+
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Rebecca'}
 
